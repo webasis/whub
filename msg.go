bfd3611bc2edb960928689aefe6c0b45a6b458db
mofon <immofon@163.com>
2019-12-30 22:07:20 +0800

    add Record.Bind

--- msg.go
+++ msg.go
@@ -1,5 +1,11 @@
 package whub
 
+import (
+	"reflect"
+	"strconv"
+	"strings"
+)
+
 type Record map[string]string
 type Message map[string]Record
 
@@ -37,4 +43,66 @@ func (r Record) V(key string) string {
 
 func (r Record) Exist(key string) bool {
 	return r != nil && r[key] != ""
+}
+
+func (r Record) Bind(v interface{}) {
+	t := reflect.TypeOf(v)
+	if t.Kind() != reflect.Ptr {
+		panic("expect pointer of struct")
+	}
+	t = t.Elem()
+	if t.Kind() != reflect.Struct {
+		panic("expect pointer of struct")
+	}
+
+	rv := reflect.ValueOf(v).Elem()
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		key := strings.ToLower(f.Name)
+		tag := f.Tag.Get("whub")
+		if tag == "-" { // ignore fields with tag `whub:"-"`
+			continue
+		}
+		if tag != "" {
+			key = tag
+		}
+
+		value := r[key]
+		if value == "" {
+			continue
+		}
+
+		rvf := rv.Field(i)
+		switch f.Type.Kind() {
+		case reflect.String:
+			rvf.SetString(value)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			i64, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				continue
+			}
+			rvf.SetInt(i64)
+		case reflect.Float32, reflect.Float64:
+			f64, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				continue
+			}
+			rvf.SetFloat(f64)
+		case reflect.Bool:
+			value = strings.ToLower(value)
+			switch value {
+			case "true", "t":
+				rvf.SetBool(true)
+			case "false", "f":
+				rvf.SetBool(false)
+			}
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			ui64, err := strconv.ParseUint(value, 10, 64)
+			if err != nil {
+				continue
+			}
+			rvf.SetUint(ui64)
+		}
+
+	}
 }
--- msg_test.go
+++ msg_test.go
@@ -29,4 +29,39 @@ func Test_Message(t *testing.T) {
 			t.Fatal(i, c.scope, c.key, c.value)
 		}
 	}
+	t.Log("OK")
+}
+
+func Test_Record_Bind(t *testing.T) {
+	type Req struct {
+		Name   string `whub:"-"`
+		Age    int32
+		Length float64
+		OK     bool
+		Url    string
+	}
+	var r Req
+	R().
+		Put("name", "mofon").
+		Put("url", "https://mofon.top/").
+		Put("age", "18").
+		Put("length", "-2").
+		Put("ok", "true").
+		Bind(&r)
+
+	if r.Name != "" {
+		t.Fatal(r)
+	}
+	if r.Url != "https://mofon.top/" {
+		t.Fatal(r)
+	}
+	if r.Age != 18 {
+		t.Fatal(r)
+	}
+	if r.Length != -2 {
+		t.Fatal(r)
+	}
+	if r.OK != true {
+		t.Fatal(r)
+	}
 }
